8210e9d166b0a6dd4870315ad9217e34f94c1d0f
ukiline <345004163@qq.com>
2023-04-02 20:55:24 +0800

    提交文件

---
+++ face-web-flask/stage2/tempCodeRunnerFile.py
@@ -0,0 +1,4 @@
+tb_logdir = r"D:\Face_search\face-web-flask\stage2\run"
+rgb_dir = r'D:\Face_search\face-web-flask\stage2\__pycache__'
+backbone_model_dir = r'D:\Face_search\face-web-flask\model_backbone_.pth'
+attn_model_dir = r'D:\Face_search\face-web-flask\model_attn_.pth'
